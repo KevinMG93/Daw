e33818ed27a151613bb2d2e625277468d736b0ac
Kevin Melia <kevin-melia-93@hotmail.com>
2014-02-17 09:55:51 +0100

    Proyecto

---
+++ Proyecto 1/src/Equipo.java
@@ -0,0 +1,17 @@
+
+public class Equipo {
+	
+	//Atributos
+	 private String nombreEquipo;
+	 private int golesFavor;
+	 private int golesEnContra;
+	 private int partidosGanados;
+	 private int partidosPerdidos;
+	 
+	public Equipo() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+}
